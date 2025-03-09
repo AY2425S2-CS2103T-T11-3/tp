2a9be49efaa16c1e98354b2005e699ca2df5c1c4
Jee <hengjeekuan@gmail.com>
2025-03-09 18:08:56 +0800

    Add AddExternalCommand class

---
+++ src/main/java/seedu/address/logic/commands/AddExternalCommand.java
@@ -0,0 +1,68 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.*;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+public class AddExternalCommand extends Command {
+
+    public static final String COMMAND_WORD = "add_ext";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds an external party to the address book. "
+            + "Parameters: "
+            + PREFIX_NAME + "NAME "
+            + PREFIX_PHONE + "PHONE "
+            + PREFIX_EMAIL + "EMAIL "
+            + PREFIX_DESCRIPTION + "DESCRIPTION "
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_NAME + "John Doe "
+            + PREFIX_PHONE + "98765432 "
+            + PREFIX_EMAIL + "johnd@example.com "
+            + PREFIX_DESCRIPTION + "External party for food. ";
+
+    public static final String MESSAGE_SUCCESS = "New external party added: %1$s";
+    public static final String MESSAGE_DUPLICATE_PARTY = "This external party already exists in ResiConnect";
+
+    private final Person toAdd;
+
+    /**
+     * Creates an AddExternalCommand to add the specified {@code Person}
+     */
+    public AddExternalCommand(Person person) {
+        requireNonNull(person);
+        toAdd = person;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        return new CommandResult("Add External Party Command", false, false);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof AddExternalCommand)) {
+            return false;
+        }
+
+        AddExternalCommand otherAddCommand = (AddExternalCommand) other;
+        return toAdd.equals(otherAddCommand.toAdd);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("toAdd", toAdd)
+                .toString();
+    }
+}
