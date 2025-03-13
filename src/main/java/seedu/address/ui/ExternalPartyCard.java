bdcea25c1f51bc63ba5fd9507a7c41a44c0b7e4a
Jee <hengjeekuan@gmail.com>
2025-03-13 18:37:00 +0800

    Create ExternalPartyCard.java

---
+++ src/main/java/seedu/address/ui/ExternalPartyCard.java
@@ -0,0 +1,48 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import seedu.address.model.person.ExternalParty;
+
+/**
+ * A UI component that displays information of a {@code ExternalParty}.
+ */
+public class ExternalPartyCard {
+
+    private static final String FXML = "ExternalParty.fxml";
+
+    public final ExternalParty externalParty;
+
+    @FXML
+    private final Label externalPartyLabel = new Label("ExternalParty");
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label name;
+    @FXML
+    private Label id;
+    @FXML
+    private Label phone;
+    @FXML
+    private Label address;
+    @FXML
+    private Label email;
+    @FXML
+    private Label description;
+
+    /**
+     * Creates a {@code ExternalParty} with the given {@code ExternalParty} and index to display.
+     */
+    public ExternalPartyCard(ExternalParty externalParty, int displayedIndex) {
+        super(FXML);
+        this.externalParty = externalParty;
+
+        id.setText(displayedIndex + ". ");
+        name.setText(externalParty.getName().fullName);
+        phone.setText(externalParty.getPhone().value);
+        email.setText(externalParty.getEmail().value);
+        description.setText(externalParty.getDescription().value);
+    }
+
+}
