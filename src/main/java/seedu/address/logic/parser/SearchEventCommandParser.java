35856d331a2f836fe2f12638b1b553837850b32b
blacksesamezongzi <sunjiaen666@gmail.com>
2025-03-13 22:46:59 +0800

    Update import order in SearchEventCommandParser.java

--- src/main/java/seedu/address/logic/parser/SearchEventCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SearchEventCommandParser.java
@@ -1,8 +1,8 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EVENT_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EVENT_END_TIME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EVENT_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EVENT_START_TIME;
 
 import java.util.Optional;
