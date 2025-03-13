d7f76b31820c9a7582d614a3a8535dcbaf3a72da
hengjeekuan <156592629+hengjeekuan@users.noreply.github.com>
2025-03-13 10:36:58 +0800

    Update CliSyntax.java

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -9,8 +9,8 @@ public class CliSyntax {
     public static final Prefix PREFIX_NAME = new Prefix("name/");
     public static final Prefix PREFIX_PHONE = new Prefix("phone/");
     public static final Prefix PREFIX_EMAIL = new Prefix("email/");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("address/");
-    public static final Prefix PREFIX_TAG = new Prefix("tag/");
+    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
+    public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_DESCRIPTION = new Prefix("desc/");
     public static final Prefix PREFIX_MATRIC = new Prefix("matric/");
     public static final Prefix PREFIX_EMERGENCY = new Prefix("emergency/");
