2aa6b5e2e273532d59a493d8b50907fbb905a9e0
chinpcbenjamin <chinpcbenjamin@gmail.com>
2025-03-16 22:35:19 +0800

    Add DeleteStudentCommand class

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -17,6 +17,8 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX =
             "The person index provided is invalid";
+    public static final String MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX =
+            "The student index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_NO_STAFF_FOUND =
             "Found no matching staff with the attributes: %1$s";
