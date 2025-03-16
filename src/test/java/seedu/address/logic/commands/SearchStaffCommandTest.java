afb551c50efafe3ce9d0743846ad3c914a1e7fa9
mhaikelll <126130091+mhaikelll@users.noreply.github.com>
2025-03-16 19:49:32 +0800

    Add testing for add student, search staff, search student v4

--- src/test/java/seedu/address/logic/commands/SearchStaffCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SearchStaffCommandTest.java
@@ -1,5 +1,51 @@
 package seedu.address.logic.commands;
 
-public class SearchStaffCommandTest {
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.parser.Prefix;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.logic.parser.CliSyntax;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code SearchStaffCommand}.
+ */
+public class SearchStaffCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void equals() {
+        Map<Prefix, String> firstCriteria = new HashMap<>();
+        firstCriteria.put(CliSyntax.PREFIX_NAME, "Alice");
+        Map<Prefix, String> secondCriteria = new HashMap<>();
+        secondCriteria.put(CliSyntax.PREFIX_PHONE, "98765432");
+
+        SearchStaffCommand searchFirstCommand = new SearchStaffCommand(firstCriteria);
+        SearchStaffCommand searchSecondCommand = new SearchStaffCommand(secondCriteria);
+
+        // same object -> returns true
+        assertTrue(searchFirstCommand.equals(searchFirstCommand));
+
+        // same values -> returns true
+        SearchStaffCommand searchFirstCommandCopy = new SearchStaffCommand(firstCriteria);
+        assertTrue(searchFirstCommand.equals(searchFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(searchFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(searchFirstCommand.equals(null));
+
+        // different search criteria -> returns false
+        assertFalse(searchFirstCommand.equals(searchSecondCommand));
+    }
 }
--- src/test/java/seedu/address/logic/commands/SearchStudentCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SearchStudentCommandTest.java
@@ -1,5 +1,51 @@
 package seedu.address.logic.commands;
 
-public class SearchStudentCommandTest {
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.parser.Prefix;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.logic.parser.CliSyntax;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code SearchStudentCommand}.
+ */
+public class SearchStudentCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void equals() {
+        Map<Prefix, String> firstCriteria = new HashMap<>();
+        firstCriteria.put(CliSyntax.PREFIX_NAME, "Alice");
+        Map<Prefix, String> secondCriteria = new HashMap<>();
+        secondCriteria.put(CliSyntax.PREFIX_PHONE, "98765432");
+
+        SearchStudentCommand searchFirstCommand = new SearchStudentCommand(firstCriteria);
+        SearchStudentCommand searchSecondCommand = new SearchStudentCommand(secondCriteria);
+
+        // same object -> returns true
+        assertTrue(searchFirstCommand.equals(searchFirstCommand));
+
+        // same values -> returns true
+        SearchStudentCommand searchFirstCommandCopy = new SearchStudentCommand(firstCriteria);
+        assertTrue(searchFirstCommand.equals(searchFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(searchFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(searchFirstCommand.equals(null));
+
+        // different search criteria -> returns false
+        assertFalse(searchFirstCommand.equals(searchSecondCommand));
+    }
 }
