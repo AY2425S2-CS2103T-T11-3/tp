cabf2b6fcd37d4f826fa8edcbfcb72935394484e
mhaikelll <126130091+mhaikelll@users.noreply.github.com>
2025-03-16 20:19:38 +0800

    Add testing for add student, search staff, search student v9

--- src/test/java/seedu/address/model/person/StudentMatchesAttributesPredicateTest.java
+++ src/test/java/seedu/address/model/person/StudentMatchesAttributesPredicateTest.java
@@ -2,7 +2,6 @@ package seedu.address.model.person;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import static seedu.address.logic.parser.CliSyntax.PREFIX_LEVEL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MATRIC;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
