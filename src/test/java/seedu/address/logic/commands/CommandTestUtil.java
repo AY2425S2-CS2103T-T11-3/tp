5a128896107f097bb169586adbc6f2761425af72
Jee <hengjeekuan@gmail.com>
2025-03-25 16:58:07 +0800

    Add new test cases

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -42,6 +42,11 @@ public class CommandTestUtil {
     public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
+    public static final String VALID_BLOCK_BOB = "B";
+    public static final String VALID_LEVEL_BOB = "1";
+    public static final String VALID_ROOM_BOB = "1";
+    public static final String VALID_DESIGNATION_BOB = "1";
+
 
     public static final String DESCRIPTION_DESC_BOB = " " + PREFIX_DESCRIPTION + VALID_DESCRIPTION_BOB;
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
--- src/test/java/seedu/address/model/person/StaffTest.java
+++ src/test/java/seedu/address/model/person/StaffTest.java
@@ -3,9 +3,15 @@ package seedu.address.model.person;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_BLOCK_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DESIGNATION_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_LEVEL_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ROOM_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 
 import org.junit.jupiter.api.Test;
 
@@ -23,23 +29,46 @@ public class StaffTest {
         // null -> returns false
         assertFalse(staff.isSamePerson(null));
 
-        // same name, all other attributes different -> returns true
-        Staff editedStaff = new StaffBuilder().withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB)
-                .withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND).build();
+        // same phone, all other attributes different -> returns true
+        Staff editedStaff = new StaffBuilder()
+                .withName(VALID_NAME_BOB)
+                .withEmail(VALID_EMAIL_BOB)
+                .withAddress(VALID_ADDRESS_BOB)
+                .withTags(VALID_TAG_FRIEND)
+                .withEmergency(VALID_PHONE_AMY)
+                .withBlock(VALID_BLOCK_BOB)
+                .withLevel(VALID_LEVEL_BOB)
+                .withRoom(VALID_ROOM_BOB)
+                .withStaffDesignation(VALID_DESIGNATION_BOB)
+                .build();
         assertTrue(staff.isSamePerson(editedStaff));
 
-        // different name, all other attributes same -> returns false
-        editedStaff = new StaffBuilder().withName("John").build();
-        assertFalse(staff.isSamePerson(editedStaff));
+        // same email, all other attributes different -> returns true
+        editedStaff = new StaffBuilder()
+                .withName(VALID_NAME_BOB)
+                .withPhone(VALID_PHONE_BOB)
+                .withAddress(VALID_ADDRESS_BOB)
+                .withTags(VALID_TAG_FRIEND)
+                .withEmergency(VALID_PHONE_AMY)
+                .withBlock(VALID_BLOCK_BOB)
+                .withLevel(VALID_LEVEL_BOB)
+                .withRoom(VALID_ROOM_BOB)
+                .withStaffDesignation(VALID_DESIGNATION_BOB)
+                .build();
+        assertTrue(staff.isSamePerson(editedStaff));
 
-        // name differs in case, all other attributes same -> returns false
-        editedStaff = new StaffBuilder().withName(StaffBuilder.DEFAULT_NAME.toLowerCase()).build();
-        assertFalse(staff.isSamePerson(editedStaff));
-
-        // name has trailing spaces, all other attributes same -> returns false
-        String nameWithTrailingSpaces = StaffBuilder.DEFAULT_NAME + " ";
-        editedStaff = new StaffBuilder().withName(nameWithTrailingSpaces).build();
-        assertFalse(staff.isSamePerson(editedStaff));
+        // same phone and email, all other attributes different -> returns true
+        editedStaff = new StaffBuilder()
+                .withName(VALID_NAME_BOB)
+                .withAddress(VALID_ADDRESS_BOB)
+                .withTags(VALID_TAG_FRIEND)
+                .withEmergency(VALID_PHONE_AMY)
+                .withBlock(VALID_BLOCK_BOB)
+                .withLevel(VALID_LEVEL_BOB)
+                .withRoom(VALID_ROOM_BOB)
+                .withStaffDesignation(VALID_DESIGNATION_BOB)
+                .build();
+        assertTrue(staff.isSamePerson(editedStaff));
     }
 
     @Test
