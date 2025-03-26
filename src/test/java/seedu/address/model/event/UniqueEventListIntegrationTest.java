4f30f94e5fc7fc0f0c911fb7e3d6c4df44b8b1f5
mhaikelll <126130091+mhaikelll@users.noreply.github.com>
2025-03-27 00:14:47 +0800

    Fix import order

--- src/test/java/seedu/address/model/event/UniqueEventListIntegrationTest.java
+++ src/test/java/seedu/address/model/event/UniqueEventListIntegrationTest.java
@@ -3,8 +3,8 @@ package seedu.address.model.event;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalEvents.DANCE_EVENT;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalExternalParties.JESSICA;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalStaffs.HARIS;
 
 import org.junit.jupiter.api.BeforeEach;
