f4df4368cf3481afb4ff83f3fb15345755d73668
chinpcbenjamin <chinpcbenjamin@gmail.com>
2025-04-01 17:47:11 +0800

    Update checkstyle

--- src/main/java/seedu/address/logic/commands/AddStaffCommand.java
+++ src/main/java/seedu/address/logic/commands/AddStaffCommand.java
@@ -16,7 +16,6 @@ import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.Person;
 import seedu.address.model.person.Staff;
 
 /**
