1d89f8aec67136f68fe25fdaddb6271120d54b6e
chinpcbenjamin <chinpcbenjamin@gmail.com>
2025-04-01 18:04:25 +0800

    Update AddExternalPartyCommandIntegrationTest

--- src/test/java/seedu/address/logic/commands/AddExternalPartyCommandIntegrationTest.java
+++ src/test/java/seedu/address/logic/commands/AddExternalPartyCommandIntegrationTest.java
@@ -8,11 +8,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.ExternalParty;
+import seedu.address.testutil.Assert;
 import seedu.address.testutil.ExternalPartyBuilder;
+import seedu.address.testutil.StaffBuilder;
+import seedu.address.testutil.StudentBuilder;
 
 public class AddExternalPartyCommandIntegrationTest {
     private Model model;
@@ -40,4 +45,49 @@ public class AddExternalPartyCommandIntegrationTest {
         assertCommandFailure(new AddExternalPartyCommand(externalPartyInList), model,
                 AddExternalPartyCommand.MESSAGE_DUPLICATE_PARTY);
     }
+
+    @Test
+    public void execute_staffWithSameEmail_throwsCommandException() {
+        model = new ModelManager(new AddressBook(), new UserPrefs());
+
+        model.addStaff(new StaffBuilder().withEmail("hey@example.com").build());
+
+        AddExternalPartyCommand command = new AddExternalPartyCommand(new ExternalPartyBuilder().withEmail("hey@example.com").build());
+        Assert.assertThrows(CommandException.class,
+                Messages.MESSAGE_DUPLICATE_PHONE_OR_EMAIL, () -> command.execute(model));
+    }
+
+    @Test
+    public void execute_staffWithSamePhone_throwsCommandException() {
+        model = new ModelManager(new AddressBook(), new UserPrefs());
+
+        model.addStaff(new StaffBuilder().withPhone("9999").build());
+
+        AddExternalPartyCommand command = new AddExternalPartyCommand(new ExternalPartyBuilder().withPhone("9999").build());
+        Assert.assertThrows(CommandException.class,
+                Messages.MESSAGE_DUPLICATE_PHONE_OR_EMAIL, () -> command.execute(model));
+    }
+
+    @Test
+    public void execute_studentWithSameEmail_throwsCommandException() {
+        model = new ModelManager(new AddressBook(), new UserPrefs());
+
+        model.addStudent(new StudentBuilder().withEmail("hey@example.com").build());
+
+        AddExternalPartyCommand command = new AddExternalPartyCommand(new ExternalPartyBuilder().withEmail("hey@example.com").build());
+        Assert.assertThrows(CommandException.class,
+                Messages.MESSAGE_DUPLICATE_PHONE_OR_EMAIL, () -> command.execute(model));
+    }
+
+    @Test
+    public void execute_studentWithSamePhone_throwsCommandException() {
+        model = new ModelManager(new AddressBook(), new UserPrefs());
+
+        model.addStudent(new StudentBuilder().withPhone("9999").build());
+
+        AddExternalPartyCommand command = new AddExternalPartyCommand(new ExternalPartyBuilder().withPhone("9999").build());
+        Assert.assertThrows(CommandException.class,
+                Messages.MESSAGE_DUPLICATE_PHONE_OR_EMAIL, () -> command.execute(model));
+    }
+
 }
