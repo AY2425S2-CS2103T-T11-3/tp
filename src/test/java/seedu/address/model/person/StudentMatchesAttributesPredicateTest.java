0845102516532c289b79a19fb6dcf7dc80199f44
mhaikelll <126130091+mhaikelll@users.noreply.github.com>
2025-04-02 15:16:07 +0800

    Update tests for tagging

--- src/test/java/seedu/address/model/person/StudentMatchesAttributesPredicateTest.java
+++ src/test/java/seedu/address/model/person/StudentMatchesAttributesPredicateTest.java
@@ -2,11 +2,13 @@ package seedu.address.model.person;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DESIGNATION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_LEVEL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MATRIC;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ROOM;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -73,5 +75,37 @@ public class StudentMatchesAttributesPredicateTest {
         criteria.put(PREFIX_ROOM, "1");
         StudentMatchesAttributesPredicate predicate = new StudentMatchesAttributesPredicate(criteria);
         assertFalse(predicate.test(new StudentBuilder().withRoom("2").build()));
+    }
+
+    @Test
+    public void test_tagValid_returnsTrue() {
+        Map<Prefix, String> criteria = new HashMap<>();
+        criteria.put(PREFIX_TAG, "friend");
+        StudentMatchesAttributesPredicate predicate = new StudentMatchesAttributesPredicate(criteria);
+        assertTrue(predicate.test(new StudentBuilder().withTags("friend").build()));
+    }
+
+    @Test
+    public void test_designationIndexOutOfBounds_returnsFalse() {
+        Map<Prefix, String> criteria = new HashMap<>();
+        criteria.put(PREFIX_DESIGNATION, "3");
+        StudentMatchesAttributesPredicate predicate = new StudentMatchesAttributesPredicate(criteria);
+        assertFalse(predicate.test(new StudentBuilder().withDesignation("2").build()));
+    }
+
+    @Test
+    public void test_designationIncorrectIndex_returnsFalse() {
+        Map<Prefix, String> criteria = new HashMap<>();
+        criteria.put(PREFIX_DESIGNATION, "0");
+        StudentMatchesAttributesPredicate predicate = new StudentMatchesAttributesPredicate(criteria);
+        assertFalse(predicate.test(new StudentBuilder().withDesignation("2").build()));
+    }
+
+    @Test
+    public void test_designationValidIndex_returnsTrue() {
+        Map<Prefix, String> criteria = new HashMap<>();
+        criteria.put(PREFIX_DESIGNATION, "1");
+        StudentMatchesAttributesPredicate predicate = new StudentMatchesAttributesPredicate(criteria);
+        assertTrue(predicate.test(new StudentBuilder().withDesignation("1").build()));
     }
 }
